2243142fbc687085786bbaa7f7e476efa9be35d8
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-09-15 17:13:42 +0200

    added multiple obstacles

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 class Game {
-    constructor() {
+    constructor(){
         this.player = null; //will store an instance of the class Player
+        this.obstacles = [];
     }
-    start() {
-        // const player = new Player wouldn't live in other methods
-        this.player = new Player(); // this way player is available everywhere
+    start(){
+        this.player = new Player();
         this.attachEventListeners();
+        
+        setInterval(() => {
+            this.obstacles.forEach((obstacleInstance) => {
+                obstacleInstance.moveDown();
+            })
+        }, 60);
+
+        setInterval(() => {
+            //create new obstacle
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        }, 3000);
+
     }
-    attachEventListeners() {
+    attachEventListeners(){
         document.addEventListener('keydown', (event) => {
-            if(event.key === 'Arrowleft') {
+            if(event.key === 'ArrowLeft'){
                 this.player.moveLeft();
-            } else if(event.key === 'Arrowright') {
+            }else if(event.key === 'ArrowRight'){
                 this.player.moveRight();
             }
         });
     }
 }
+
 
 class Player {
     constructor(){
@@ -28,32 +42,62 @@ class Player {
 
         this.createDomElement();
     }
-    createDomElement() {
+    createDomElement(){
+        // create dom element
         this.domElement = document.createElement('div');
-        
-        this.domElement.id = "player";
+
+        // set id and css
+        this.domElement.id = 'player';
         this.domElement.style.width = this.width + 'vw';
         this.domElement.style.height = this.height + 'vh';
         this.domElement.style.bottom = this.positionY + 'vh';
         this.domElement.style.left = this.positionX + 'vw';
 
+        // append to the dom
         const boardElm = document.getElementById('board');
         boardElm.appendChild(this.domElement)
-
-
-
     }
-    moveLeft() {
-        this.positionX--
-        this.domElement.style.left = this.positionX = 'vw';
+    moveLeft(){
+        this.positionX--;
+        this.domElement.style.left = this.positionX + 'vw';
     }
-    moveRight() {
+    moveRight(){
         this.positionX++;
-        this.domElement.style.left = this.positionX = 'vw';
-
+        this.domElement.style.left = this.positionX + 'vw';
     }
 }
 
 
-const funGame = new Game();
-funGame.start();
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.className = 'obstacle';
+        this.domElement.style.width = this.width + 'vw';
+        this.domElement.style.height = this.height + 'vh';
+        this.domElement.style.bottom = this.positionY + 'vh';
+        this.domElement.style.left = this.positionX + 'vw';
+
+        // append to the dom
+        const boardElm = document.getElementById('board');
+        boardElm.appendChild(this.domElement)
+    }
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + 'vh';
+    }
+}
+
+const game = new Game();
+game.start();
